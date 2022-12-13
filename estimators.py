5b72921119cbd1eb5585d5c193804d8a8b63d63b
benmor20 <benmor2020@gmail.com>
2022-12-12 20:48:04 -0500

    Estimator

---
+++ estimators.py
@@ -0,0 +1,108 @@
+"""
+Module to deal with stat estimation
+"""
+from abc import ABC, abstractmethod
+from typing import *
+import numpy as np
+
+
+class Estimator(ABC):
+    """
+    Class to estimate a certain statistic
+
+    Attributes:
+        current_estimate: a numpy vector giving the probability of each option
+        noptions: int, the number of options
+    """
+    def __init__(self, noptions: int):
+        self.current_estimate = np.ones((noptions,)) / noptions
+
+    @property
+    def noptions(self) -> int:
+        return self.current_estimate.shape[0]
+
+    @property
+    def xvals(self) -> np.ndarray:
+        return np.arange(self.noptions)
+
+    def set_prior(self, prior: np.ndarray):
+        """
+        Sets the prior distribution
+
+        :param prior: an ndarray, the prior distribution
+        """
+        assert sum(prior) == 1
+        self.current_estimate = prior
+
+    @abstractmethod
+    def model(self, measurement) -> np.ndarray:
+        """
+        Calculates the probability of the measurement for each option
+
+        :param measurement: the most recent measurement
+        :return: a numpy vector with (noptions) elements, giving the probability of this measurement given the
+            estimated value
+        """
+        pass
+
+    def update(self, measurement):
+        """
+        Update the current belief given a measurement
+
+        :param measurement: the measured value
+        """
+        self.current_estimate *= self.model(measurement)
+        self.current_estimate /= np.sum(self.current_estimate)
+
+    def map(self) -> float:
+        """
+        :return: a float, the Maximum A Posteriori (or the average of them in the case of multiple) of the current estimate
+        """
+        highest = np.max(self.current_estimate)
+        modes = self.current_estimate == highest
+        return np.sum(self.current_estimate[modes]) / len(modes)
+
+    def mean(self) -> float:
+        """
+        :return: a float, the weighted mean of the current estimate
+        """
+        return sum(self.current_estimate * np.arange(self.current_estimate.shape[0]))
+
+
+class ModifierEstimator(Estimator):
+    def __init__(self):
+        super().__init__(21)  # -5 to 15
+
+    @property
+    def xvals(self) -> np.ndarray:
+        return np.arange(self.noptions) - 5
+
+    def model(self, measurement: int) -> np.ndarray:
+        """
+        Calculates the probability of the measurement for each option
+
+        :param measurement: an int, the most recent roll
+        :return: a numpy vector with (noptions) elements, giving the probability of this measurement given the
+            estimated value
+        """
+        poss_rolls = measurement - self.xvals
+        in_range = (1 <= poss_rolls) & (poss_rolls <= 20)
+        return in_range / 20
+
+
+class ACEstimator(Estimator):
+    def __init__(self):
+        super().__init__(31)  # 0 to 30
+
+    def model(self, measurement: Tuple[int, bool]):
+        """
+        Calculates the probability of the measurement for each option
+
+        :param measurement: a tuple - the most recent roll followed by whether it hit
+        :return: a numpy vector with (noptions) elements, giving the probability of this measurement given the
+            estimated value
+        """
+        roll, hit = measurement
+        if hit:
+            return self.xvals <= roll
+        return self.xvals > roll
--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import random
+from matplotlib import pyplot as plt
+import numpy as np
 
 from characters import Character
+from estimators import *
+
+
+def display_plots(mod_est, ac_est):
+    fig, axs = plt.subplots(2, len(mod_est))
+    for idx, (name, est) in enumerate(mod_est.items()):
+        axs[0, idx].hist(est.xvals, bins=est.noptions, weights=est.current_estimate)
+        axs[0, idx].set(xlabel='Modifier')
+        axs[0, idx].set_title(name)
+        if idx == 0:
+            axs[0, idx].set(ylabel='Probability')
+    for idx, (name, est) in enumerate(ac_est.items()):
+        axs[1, idx].hist(est.xvals, bins=est.noptions, weights=est.current_estimate)
+        axs[1, idx].set(xlabel='AC')
+        if idx == 0:
+            axs[1, idx].set(ylabel='Probability')
+    plt.show()
 
 
 def main():
     party1 = [Character('Manster Wipower', 30, 10, 10, 0, 8, False), Character('Abayes Satano\'brien', 50, 14, 10, 0, 10, False)]
     party2 = [Character('Blelduth Chestsplitter', 60, 14, 10, 0, 8, False), Character('Washingtonlotus Toadstallchardson', 30, 8, 14, 0, 8, True)]
+    mod_est = {c.name: ModifierEstimator() for c in party2}
+    ac_est = {c.name: ACEstimator() for c in party2}
     all_characters = {c.name: c for c in party1 + party2}
     initiative = {n: c.roll_initiative() for n, c in all_characters.items()}
     initiative_order = sorted(all_characters.keys(), key=lambda i: initiative[i], reverse=True)
@@ -14,20 +35,28 @@ def main():
     while True:
         for name in initiative_order:
             character = all_characters[name]
+            is_party1 = any(c.name == name for c in party1)
             if character.hp <= 0:
                 continue
-            other_party = party2 if any(c.name == name for c in party1) else party1
+            other_party = party2 if is_party1 else party1
             opponent = random.choice(other_party)
             while opponent.hp <= 0:
                 opponent = random.choice(other_party)
 
             to_hit = character.roll_to_hit()
-            if to_hit >= opponent.ac:
+            hit = to_hit >= opponent.ac
+            if hit:
                 damage = character.roll_damage()
                 opponent.hp -= damage
                 print(f'{name} hit {opponent.name} for {damage} damage (they have {opponent.hp} HP left)')
                 if opponent.hp <= 0:
                     print(f'{opponent.name} is DEAD')
+
+            if is_party1:
+                ac_est[opponent.name].update((to_hit, hit))
+            else:
+                mod_est[name].update(to_hit)
+        display_plots(mod_est, ac_est)
         if all(c.hp <= 0 for c in party1):
             print('Freak the Mighty wins!')
             break
